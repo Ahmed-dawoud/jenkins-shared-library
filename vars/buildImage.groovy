94a60725486584d1e2814b48f450ac6cdd1c50fd
root <root@EPUAKYIW0C58.kyiv.epam.com>
2021-08-27 19:45:41 +0300

    add variable for image name

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "Building the Docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 207.154.204.232:8083/java-maven-app:2.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin 207.154.204.232:8083"
-        sh 'docker push 207.154.204.232:8083/java-maven-app:2.0'
+        sh "docker push $imageName"
     }
 }
